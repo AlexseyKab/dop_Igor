da0bdc1d89aacef23fde821bf4961b6a940473cd
AlexseyKab <help_me101@mail.ru>
2022-10-29 16:32:35 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,49 @@
+import React, {useEffect, useState} from 'react';
+import './App.css';
+import {Button} from "./components/Button";
+
+type ShowType = {
+  completed: boolean
+  id: number
+  title: string
+  userId: number
+}
+
+function App() {
+
+  const [show, setShow] = useState<Array<ShowType>>([])
+
+  const useShowUp = () => {
+   /* useEffect(() => {*/
+      fetch('https://jsonplaceholder.typicode.com/todos')
+          .then(response => response.json())
+          .then(json => setShow(json))
+  /*  }, [])*/
+  }
+
+  const clearMeHandler = () => {
+      setShow([])
+  }
+
+  return (
+    <div className="App">
+      <Button name={'Show me'} callback={useShowUp}/>
+      <Button name={'Clear me'} callback={clearMeHandler}/>
+      <ul>
+        {
+          show.map(el => {
+            return (
+            <li key={el.id}>
+              <input type="checkbox" checked={el.completed}/>
+              <span>{el.title}</span>
+              <span>{el.userId}</span>
+            </li>
+            )
+          })
+        }
+      </ul>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Button.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+type ButtonType = {
+    name: string
+    callback: () => void
+}
+
+export const Button = (props: ButtonType) => {
+
+    const {name, callback} = props
+
+    const onClickHandler = () => {
+        callback()
+    }
+
+    return (
+        <button onClick={onClickHandler}>{name}</button>
+    );
+};
